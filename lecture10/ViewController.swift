c64ac55fbc19b7ae4bb433c9fea2986a143429bf
Monica Ong <myo3@cornell.edu>
2017-11-27 20:02:24 -0500

    Created lecture 10 project and gitignore

---
+++ lecture10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lecture10
+//
+//  Created by Monica Ong on 11/27/17.
+//  Copyright © 2017 CS 1998. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        title = "Lecture 10"
+        view.backgroundColor = .blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
